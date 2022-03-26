2b5be9c8ec387b87566fc1614aba776348e1572d
Junaedi Akbar <juned.akb@gmail.com>
2022-03-27 00:50:09 +0700

    fix: api-key to local.properties

--- app/src/main/java/com/juned/githubuserssearch/api/ApiService.kt
+++ app/src/main/java/com/juned/githubuserssearch/api/ApiService.kt
@@ -6,21 +6,21 @@ import retrofit2.http.*
 
 interface ApiService {
 
-    @Headers("Authorization:" + BuildConfig.KEY)
+    @Headers("Authorization:" + BuildConfig.API_KEY)
     @GET("search/users")
     fun getUsers(@Query("q") keyword:String,
                @Query("per_page") per_page:Int=15,
                @Query("page") page:Int=1):Call<UsersResponse>?
 
-    @Headers("Authorization:" + BuildConfig.KEY)
+    @Headers("Authorization:" + BuildConfig.API_KEY)
     @GET("/users/{username}")
     fun getUserDetail(@Path("username") username:String) : Call<UserDetailsResponse>?
 
-    @Headers("Authorization:" + BuildConfig.KEY)
+    @Headers("Authorization:" + BuildConfig.API_KEY)
     @GET("users/{username}/following")
     fun getFollowingUser(@Path("username") username:String):Call<List<ListUsersResponse>>?
 
-    @Headers("Authorization:" + BuildConfig.KEY)
+    @Headers("Authorization:" + BuildConfig.API_KEY)
     @GET("users/{username}/followers")
     fun getFollowersUser(@Path("username") username:String):Call<List<ListUsersResponse>>?
 }
